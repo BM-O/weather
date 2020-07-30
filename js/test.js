c4d496f43e1a10a74f799f5f18a5d6b79992a452
MQZON <bmo@pdx.edu>
2020-07-30 14:46:47 -0700

    Minor tweaks

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 function myMove() {
     var elem = document.getElementById("inner");
     var pos = 0;
-    var id = setInterval(frame, 5);
+    var id = setInterval(frame, 1);
     function frame() {
-        if (pos == window.screen.width) {
+        if (pos >= window.screen.width) {
             // clearInterval(id);
             pos = 0;
         } else {
-            pos++;
+            pos+=5;
             elem.style.top = pos + 'px';
             elem.style.left = pos + 'px';
         }
