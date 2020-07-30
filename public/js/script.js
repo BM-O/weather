187ee14b3dbddc3ab7833e0170079ea8698f7092
Francis Phan <phan5@pdx.edu>
2020-07-30 14:14:49 -0700

    successfully loads weather page once location is entered

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ const search = document.getElementById("get_location");
 search.addEventListener("click", () => {
   const addr = document.getElementById("address").value;
   const getCoords = firebase.functions().httpsCallable("getCoords");
-  getCoords({ address: addr }).then((result) => console.log(result.data));
+  getCoords({ address: addr }).then((result) => showWeather(result.data));
 });
 
 function showWeather(data) {
