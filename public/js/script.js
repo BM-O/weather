6967e689fb82eca84f54d2ab659ab53e1878e1f0
MQZON <bmo@pdx.edu>
2020-08-03 15:32:22 -0700

    Fixed blinking issue, sun only shows up when condition is clear

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,15 @@
 const weather = document.getElementById("weather_page");
 const search = document.getElementById("get_location");
+var addr = "Portland, OR"; //Default for animation
+
+
+function loadDefault(){
+  const getCoords = firebase.functions().httpsCallable("getCoords");
+  getCoords({ address: addr }).then((result) => showWeather(result.data));
+}
 
 search.addEventListener("click", () => {
-  const addr = document.getElementById("address").value;
+  addr = document.getElementById("address").value;
   const getCoords = firebase.functions().httpsCallable("getCoords");
   getCoords({ address: addr }).then((result) => showWeather(result.data));
 });
@@ -10,7 +17,31 @@ search.addEventListener("click", () => {
 function showWeather(data) {
   makeVisible(weather);
   console.log(data);
+  
+  //animation
+  const curAddr = addr;
+  var elem = document.getElementById("inner");
+  if(data["weather"][0]["condition"] == "Clear"){
+    var x = document.getElementById("sun");
+    x.style.display = "initial";
+  }
+  var pos = 0;
+  var id = setInterval(frame => {
+    if (addr !== curAddr){
+      console.log("Address changed from ", curAddr, " to ", addr)
+      clearInterval(id);
+      return;
+    }
+    if (pos >= window.screen.width){
+      pos = 0;
+    }else{
+      pos += data["weather"][0]["wind"];
+      elem.style.left = pos + "px";
+      elem.style.top = pos + "px";
+    }
+  },20);
 }
+
 
 function makeVisible(page) {
   page.classList.add("visible");
