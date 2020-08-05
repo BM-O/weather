2b0bb3699e70ee2216cd65a00abcf4d4a635ed95
Kaushik Chaudhary <kaushik2@pdx.edu>
2020-08-04 18:35:08 -0700

    React use case

---
+++ server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  baseUrl: {
+    protocol: 'http',
+    hostname: 'api.openweathermap.org',
+    path: '/data/2.5/weather',
+  },
+
+  query: {
+    name: 'q',
+    id: 'id',
+    coordinates: {
+      latitude: 'lat',
+      longitude: 'lon',
+    },
+    zipcode: 'zip',
+  },
+
+  APIkey: '4517fa73e561d7c564d180c4aa99a9a0',
+};
